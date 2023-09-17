47e4a2032cd825c5f09401ae4d23fe901f8b0783
leandre <tuyambazeleandre@gmail.com>
2023-09-17 07:02:47 +0200

    add signout

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,19 @@
-import express from 'express';
+import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-})
+router.get("/api/users/currentuser", (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
+});
 
 export { router as currentUserRouter };
-
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there!');
-})
+router.post("/api/users/signout", (req, res) => {
+  req.session = null;
+
+  res.send({});
+});
 
 export { router as signoutRouter };
-
