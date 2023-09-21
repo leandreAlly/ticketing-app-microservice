23e1e20f9b2b319a520600c1c28065511416212e
leandre <tuyambazeleandre@gmail.com>
2023-09-21 03:52:07 +0200

    setup nextjs on kuberntes

---
+++ client/pages/banana.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banana = () => {
+  return (
+    <div>
+      <h1>Banana</h1>
+    </div>
+  );
+};
+
+export default Banana;
---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Landing Page1</h1>
+    </div>
+  );
+};
+
+export default App;
