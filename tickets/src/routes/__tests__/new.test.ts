95bcd2d6818619870d4910905f1c7a0a82c216da
leandre <tuyambazeleandre@gmail.com>
2023-10-06 16:25:38 +0200

    testing if user is signed in

--- tickets/src/routes/__tests__/new.test.ts
+++ tickets/src/routes/__tests__/new.test.ts
@@ -11,7 +11,12 @@ it("can only be accessed if the user is signed in", async () => {
 });
 
 it("returns a status other than 401 if the user is signed in", async () => {
-  const response = await request(app);
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({});
+
+  expect(response.status).not.toEqual(401);
 });
 
 it("returns an error if an invalid title is provided", async () => {});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,15 +2,16 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import request from "supertest";
 import mongoose from "mongoose";
 import { app } from "../app";
-
+import jwt from "jsonwebtoken";
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
 
 beforeAll(async () => {
   process.env.JWT_KEY = "helloworld";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
 
@@ -30,16 +31,22 @@ afterAll(async () => {
   await mongo.stop();
 });
 
-global.signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
+global.signin = () => {
+  const payload = {
+    id: "1kdyilksd234",
+    email: "test@test.com",
+  };
+  // Create JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({ email, password })
-    .expect(201);
+  // Build session object { jwt: MY_JWT }
+  const session = { jwt: token };
 
-  const cookie = response.get("Set-Cookie");
+  // Turn session into JSON
+  const sessionJSON = JSON.stringify(session);
 
-  return cookie;
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`session=${base64}`];
 };
