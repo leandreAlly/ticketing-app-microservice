41d68e66ed7761fedb29ce47b1b7f1707e34f961
leandre <tuyambazeleandre@gmail.com>
2023-10-18 07:53:52 +0200

    add orders routes and files

---
+++ orders/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@ally-tickets/common";
+import { newOrderRouter } from "./routes/new";
+import { indexOrderRouter } from "./routes";
+import { showOrderRouter } from "./routes/show";
+import { deleteOrderRouter } from "./routes/delete";
+
+const app = express();
+app.use(express.json());
+app.set("trust proxy", true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(currentUser);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
