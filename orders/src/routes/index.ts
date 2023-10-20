6b5aaa7aacd7dd5e9521fbaba1a9841ea603b23c
leandre <tuyambazeleandre@gmail.com>
2023-10-20 08:41:03 +0200

    add getting orders

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from "express";
+import { requireAuth } from "@ally-tickets/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders", (req: Request, res: Response) => {
-  res.send({});
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate("ticket");
+
+  return res.status(200).send(orders);
 });
 
 export { router as indexOrderRouter };
