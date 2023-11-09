ff980e3eaf8d183accf7acef5378634f454dc296
leandre <tuyambazeleandre@gmail.com>
2023-11-09 15:07:20 +0200

    add order creates listener

---
+++ payments/src/events/listener/__tests__/order-created-listener.test.ts
@@ -0,0 +1,46 @@
+import { OrderCreatedEvent, OrderStatus } from "@ally-tickets/common";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCreatedListener } from "../order-created-listener";
+import mongoose from "mongoose";
+import { Order } from "../../../models/order";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const data: OrderCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: "asdfkjg",
+    status: OrderStatus.Created,
+    version: 0,
+    expiresAt: "alskdfj",
+    ticket: {
+      id: "alskdfj",
+      price: 10,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it("replicates the order info", async () => {
+  const { listener, data, msg } = await setup();
+
+  //   await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order!.price).toEqual(data.ticket.price);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
---
+++ payments/src/models/order.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+import { OrderStatus } from "@ally-tickets/common";
+
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    status: {
+      type: String,
+      require: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+OrderSchema.set("versionKey", "version");
+OrderSchema.plugin(updateIfCurrentPlugin);
+
+OrderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    userId: attrs.userId,
+    price: attrs.price,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", OrderSchema);
+
+export { Order };
