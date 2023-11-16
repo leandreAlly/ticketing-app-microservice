b4980248ebab28c71158985fffd37ea4773717a7
leandre <tuyambazeleandre@gmail.com>
2023-11-17 00:07:04 +0200

    update the order status after payment completed

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -18,6 +18,8 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
 
     if (!order) throw new Error("Order not found");
 
+    if (order.status === OrderStatus.Complete) return msg.ack();
+
     order.set({
       status: OrderStatus.Cancelled,
     });
