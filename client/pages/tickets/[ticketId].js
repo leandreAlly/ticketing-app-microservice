c67e5841faf53f9098f71541f9fdf655dd228509
leandre <tuyambazeleandre@gmail.com>
2023-12-06 16:18:11 +0200

    (client): create order for ticket

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,11 +1,23 @@
 import useRequest from "../../hooks/use-request";
 
 const TicketShow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: "/api/orders",
+    method: "post",
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) => console.log(order),
+  });
+
   return (
     <div>
       <h1>{ticket.title}</h1>
       <h4>Price: ${ticket.price}</h4>
-      <button className="btn btn-primary">Purchase</button>
+      {errors}
+      <button onClick={doRequest} className="btn btn-primary">
+        Purchase
+      </button>
     </div>
   );
 };
