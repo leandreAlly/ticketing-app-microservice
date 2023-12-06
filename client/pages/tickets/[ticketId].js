e94d92955ead4c9a9da359527d6450bfa12f62b7
leandre <tuyambazeleandre@gmail.com>
2023-12-06 16:14:49 +0200

    (client): display single ticket

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,20 @@
-const TicketShow = () => {
-  return <div>Ticket Show</div>;
+import useRequest from "../../hooks/use-request";
+
+const TicketShow = ({ ticket }) => {
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: ${ticket.price}</h4>
+      <button className="btn btn-primary">Purchase</button>
+    </div>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data };
 };
 
 export default TicketShow;
