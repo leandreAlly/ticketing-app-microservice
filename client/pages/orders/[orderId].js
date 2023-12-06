d953fe2a28a928c1cacf3cd5771f507b16f8a753
leandre <tuyambazeleandre@gmail.com>
2023-12-06 18:31:08 +0200

    (client): show time remaning to pay on screen

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  if (timeLeft < 0) return <div>Order expired</div>;
+
+  return <h1>Time left to pay: {timeLeft} seconds</h1>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
