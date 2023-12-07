045b734ca8ccd6e9e163f1962e834f8294f91756
leandre <tuyambazeleandre@gmail.com>
2023-12-07 22:48:22 +0200

    (order-payment-ticket): add log when service startup

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCreatedListener } from "./events/listener/order-created-listener";
 import { OrderCancelledListener } from "./events/listener/order-cancelled-listener";
 
 const start = async () => {
+  console.log("Starting up payments service...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
