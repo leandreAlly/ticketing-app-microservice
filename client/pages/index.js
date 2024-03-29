161e6d3b8cbb7c3ee4393ed423461bee43ef5a7a
leandre <tuyambazeleandre@gmail.com>
2024-03-29 23:25:40 +0200

    remove banana page

--- client/pages/index.js
+++ client/pages/index.js
@@ -14,7 +14,7 @@ const LandingPage = ({ currentUser, tickets }) => {
   });
   return (
     <div>
-      <h2>Tickets</h2>
+      <h1>Tickets</h1>
       <table className="table">
         <thead>
           <tr>
