9dbe2dbf5bc2b9e18cc7fba2c4c617087985fad4
LEANDRE ALLY <78492995+leandreAlly@users.noreply.github.com>
2024-03-29 20:17:26 +0200

    Revert "#Test auth on  deployment on Merge"

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,27 +1,23 @@
-import mongoose from 'mongoose';
-import { app } from './app';
+import mongoose from "mongoose";
+import { app } from "./app";
 
 const start = async () => {
-<<<<<<< HEAD
   console.log("Starting up auth service....");
-=======
-  console.log('Starting up auth service... test auth depo');
->>>>>>> 3e4e3c9 (Test auth deployment on merge)
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error("JWT_KEY must be defined");
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
+    throw new Error("MONGO_URI must be defined");
   }
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to mongodb');
+    console.log("Connected to mongodb");
   } catch (err) {
     console.error(err);
   }
   app.listen(3000, () => {
-    console.log('Listening on port 3000...!!!!');
+    console.log("Listening on port 3000...!!!!");
   });
 };
 
